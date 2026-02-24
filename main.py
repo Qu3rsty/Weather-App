28a2120045a4c133da86c3c7a46c958cec062aff
Qu3rsty <qu3rsty.dev@gmail.com>
2026-02-24 22:24:47 +0100

    Added weather alerts & some small features

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from colorama import Fore, Style
 import sys
 import json
+import time
 
 # saves
 with open("cities/cities.json", "r") as file:
@@ -37,6 +38,7 @@ try:
     temp = data['main']['temp']
     humidity = data['main']['humidity']
     clouds = data['clouds']['all']
+    wind_speed = data['wind']['speed'] 
 except KeyError:
     print(Fore.RED + "Wybierz poprawne miasto!" + Style.RESET_ALL)
     sys.exit()
@@ -50,15 +52,33 @@ elif temp <= 0:
 
 print(Fore.LIGHTBLUE_EX + f"- Wilgotność powietrza: {humidity}%" + Style.RESET_ALL)
 print(Fore.WHITE + f"- Zachmurzenie: {clouds}%\n" + Style.RESET_ALL)
-sys.exit()
+time.sleep(5)
 
 # Preparing more features
+print(Fore.MAGENTA + "--- NASTEPNE OPCJE ---" + Style.RESET_ALL)
 print(Fore.MAGENTA + "(1)" + Style.RESET_ALL, "Wyjście")
+print(Fore.MAGENTA + "(2)" + Style.RESET_ALL, "Wyświetl alerty pogodowe")
 
-action_2 = input("Wybierz następne działanie\n> ")
+action_2 = input("\nWybierz następne działanie\n> ")
 
 if action_2 == '1':
     sys.exit()
+elif action_2 == '2':
+    print(Fore.MAGENTA + "--- ALERTY POGODOWE ---" + Style.RESET_ALL)
+    if temp > 30:
+        print(Fore.RED + "- Temperatura przekracza 30C!" + Style.RESET_ALL)
+    elif temp < -5:
+        print(Fore.CYAN + "- Temperatura jest niższa niż -5C!" + Style.RESET_ALL)
+    else:
+        pass
+    if wind_speed > 70:
+        print(Fore.RED + "- Prędkość wiatru przekracza 70km/h!" + Style.RESET_ALL)
+    else:
+        pass
+    if not temp > 30 and not temp < -5 and not wind_speed > 70:
+        print("Brak alertów pogodowych :D")
+    elif temp > 30 or temp < -5 and wind_speed > 70:
+        print("\nLepiej nie wychodź dzisiaj z domu")
 else:
-    print("Wybierz poprawną opcję!")
+    print(Fore.RED + "Wybierz poprawną opcję!" + Style.RESET_ALL)
     sys.exit()
