9d4280d2c2c73d6a3e2873deb52dc7a48ab1afd2
Qu3rsty <qu3rsty.dev@gmail.com>
2026-03-10 14:53:04 +0100

    Change the program language to English

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import time
 with open("cities/cities.json", "r") as file:
     cities = json.load(file)
 
-print(Fore.MAGENTA + "--- WSZYSTKIE MIASTA ---" + Style.RESET_ALL)
+print(Fore.MAGENTA + "--- All locations ---" + Style.RESET_ALL)
 
 for names in cities:
     print(Fore.MAGENTA + f"({cities[names]['id']}) " + Style.RESET_ALL + names)
 
 # City search engine
-CITY=input("\nWybierz miasto\n> ")
+CITY=input("\nSelect a town\n> ")
 
 for name, info in cities.items():
     if str(info['id']) == CITY:
@@ -40,45 +40,45 @@ try:
     clouds = data['clouds']['all']
     wind_speed = data['wind']['speed'] 
 except KeyError:
-    print(Fore.RED + "Wybierz poprawne miasto!" + Style.RESET_ALL)
+    print(Fore.RED + "Select the correct location!" + Style.RESET_ALL)
     sys.exit()
 
-print(f"\n--- Pogoda w {CITY} ---") 
+print(f"\n--- Weather in {CITY} ---") 
 
 if temp > 0:
-    print(Fore.YELLOW + f"- Temperatura: {temp} C" + Style.RESET_ALL)
+    print(Fore.YELLOW + f"- Temperature: {temp} C" + Style.RESET_ALL)
 elif temp <= 0:
-    print("> Temperatura:", Fore.CYAN + f"{temp: .0f} C" + Style.RESET_ALL)
+    print("> Temperature:", Fore.CYAN + f"{temp: .0f} C" + Style.RESET_ALL)
 
-print(Fore.LIGHTBLUE_EX + f"- Wilgotność powietrza: {humidity}%" + Style.RESET_ALL)
-print(Fore.WHITE + f"- Zachmurzenie: {clouds}%\n" + Style.RESET_ALL)
+print(Fore.LIGHTBLUE_EX + f"- Air humidity: {humidity}%" + Style.RESET_ALL)
+print(Fore.WHITE + f"- Cloudy: {clouds}%\n" + Style.RESET_ALL)
 time.sleep(5)
 
 # Preparing more features
-print(Fore.MAGENTA + "--- NASTEPNE OPCJE ---" + Style.RESET_ALL)
-print(Fore.MAGENTA + "(1)" + Style.RESET_ALL, "Wyjście")
-print(Fore.MAGENTA + "(2)" + Style.RESET_ALL, "Wyświetl alerty pogodowe")
+print(Fore.MAGENTA + "--- Next options ---" + Style.RESET_ALL)
+print(Fore.MAGENTA + "(1)" + Style.RESET_ALL, "Exit")
+print(Fore.MAGENTA + "(2)" + Style.RESET_ALL, "Show weather alerts")
 
-action_2 = input("\nWybierz następne działanie\n> ")
+action_2 = input("\nSelect next action\n> ")
 
 if action_2 == '1':
     sys.exit()
 elif action_2 == '2':
-    print(Fore.MAGENTA + "--- ALERTY POGODOWE ---" + Style.RESET_ALL)
+    print(Fore.MAGENTA + "--- Weather alerts ---" + Style.RESET_ALL)
     if temp > 30:
-        print(Fore.RED + "- Temperatura przekracza 30C!" + Style.RESET_ALL)
+        print(Fore.RED + "- The temperature exceeds 30C!" + Style.RESET_ALL)
     elif temp < -5:
-        print(Fore.CYAN + "- Temperatura jest niższa niż -5C!" + Style.RESET_ALL)
+        print(Fore.CYAN + "- The temperature is below -5C!" + Style.RESET_ALL)
     else:
         pass
     if wind_speed > 70:
-        print(Fore.RED + "- Prędkość wiatru przekracza 70km/h!" + Style.RESET_ALL)
+        print(Fore.RED + "- Wind speed exceeds 70km/h!" + Style.RESET_ALL)
     else:
         pass
     if not temp > 30 and not temp < -5 and not wind_speed > 70:
-        print("Brak alertów pogodowych :D")
+        print("No weather alerts :D")
     elif temp > 30 or temp < -5 and wind_speed > 70:
-        print("\nLepiej nie wychodź dzisiaj z domu")
+        print("\nYou better not leave the house today")
 else:
-    print(Fore.RED + "Wybierz poprawną opcję!" + Style.RESET_ALL)
+    print(Fore.RED + "Select the correct option" + Style.RESET_ALL)
     sys.exit()
